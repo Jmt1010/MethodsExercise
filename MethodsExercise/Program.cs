8aee3017ec5f2dcd53589bf27498c978e7ca053b
Jared Thompson <jaredt10@gmail.com>
2024-02-14 15:35:10 -0600

    completed method excercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your name?");
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hi, {userName}. What is your favortire color?");
+            var color = Console.ReadLine();
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal??");
+            var animal = Console.ReadLine();
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
+
+            Console.WriteLine("Thanks for the info! Compiling results now.");
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine($"Today we have learned {userName} is a big fan of the color {color}. {userName} loves seeing {animal} in the wild. Also, will never miss the chance to see {band} in concert.");
+
+            Console.WriteLine("Thank you for participating in this excercise!");
         }
     }
 }
