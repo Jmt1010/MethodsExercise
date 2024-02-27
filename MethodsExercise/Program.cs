06555efba8d4dfc0a7d8e8db66f0ddfe48320ec7
Jared Thompson <jaredt10@gmail.com>
2024-02-27 13:55:53 -0600

    completed the 2nd method exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace MethodsExercise
+﻿using System.Security.Claims;
+
+namespace MethodsExercise
 {
     public class Program
     {
+        public static int Multiply(int num1, int num2, int num3) 
+        {
+            return num1 * num2 * num3;
+        }
+        public static int Sum(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
         static void Main(string[] args)
         {
+            var wins = Sum(14, 28);
+            var points = Multiply(3, 1, wins);
+
+
+
             Console.WriteLine("Hello, what is your name?");
             var userName = Console.ReadLine();
             Console.WriteLine($"Hi, {userName}. What is your favortire color?");
@@ -16,9 +31,11 @@
             Console.WriteLine("Thanks for the info! Compiling results now.");
             Console.WriteLine("-------------------------");
 
-            Console.WriteLine($"Today we have learned {userName} is a big fan of the color {color}. {userName} loves seeing {animal} in the wild. Also, will never miss the chance to see {band} in concert.");
+            Console.WriteLine($"Today we have learned {userName} is a big fan of the color {color}. {userName} enjoys seeing a {animal} at the zoo. Also, {userName} will never miss the chance to see {band} in concert.");
 
-            Console.WriteLine("Thank you for participating in this excercise!");
+            Console.WriteLine("-------------------------");
+            
+            Console.WriteLine($"Thank you for participating in this excercise {userName}!");
         }
     }
 }
